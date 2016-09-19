cc616bd185865734c37d1797cc42e37997dd0acb
Tibor Wekerle <tibor.wekerle@CORE.LAN>
2016-09-19 17:23:25 +0300

    az osszes szint

--- src/ViewModels/LevelView.java
+++ src/ViewModels/LevelView.java
@@ -6,9 +6,13 @@
 package ViewModels;
 
 import Models.LevelModel;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
 /**
  *
@@ -30,32 +34,83 @@ public class LevelView extends GridPane{
         int height=matrix.length;
         int width=matrix[0].length;
         
-        //Button buttonAdd=new Button();
-        //buttonAdd.setGraphic(new ImageView(imageAdd));
-        
-        for(int i=0;i<height;i++)
+        int i=0,j=0;
+        for(i=0;i<height;i++)
         {
-            for(int j=0;j<width;j++)
+            for(j=0;j<width;j++)
             {
                 //# -fal
                 //a -alkololista
                 //b -nem alkoholista
-                //b -vas lada
+                //c -vas lada
+                //d -fa lada
+                //e -bomba
+                //f -tuskek
                 switch(matrix[i][j])
                 {
                     case '#':
                         Image imageWall=new Image("/img/wall.png");
-                        ImageView iv1 = new ImageView();
-                        iv1.setImage(imageWall);
-                        grid.add(iv1,j,i);
+                        ImageView imageViewWall = new ImageView();
+                        imageViewWall.setImage(imageWall);
+                        grid.add(imageViewWall,j,i);
                         break;
                     case 'a':
+                        Image imageZombie=new Image("/img/zombie.png");
+                        ImageView imageViewZombie = new ImageView();
+                        imageViewZombie.setImage(imageZombie);
+                        grid.add(imageViewZombie,j,i);
                         break;
                     case 'b':
+                        Image imagePeople=new Image("/img/people.png");
+                        ImageView imageViewPeople = new ImageView();
+                        imageViewPeople.setImage(imagePeople);
+                        grid.add(imageViewPeople,j,i);
+                        break;
+                    case 'c':
+                        Image imageMetalBox=new Image("/img/metalBox.jpg");
+                        ImageView imageViewMetalBox = new ImageView();
+                        imageViewMetalBox.setImage(imageMetalBox);
+                        grid.add(imageViewMetalBox,j,i);
+                        break;
+                    case 'd':
+                        Image imageWoodBox=new Image("/img/woodBox.png");
+                        ImageView imageViewWoodBox = new ImageView();
+                        imageViewWoodBox.setImage(imageWoodBox);
+                        grid.add(imageViewWoodBox,j,i);
+                        break;
+                    case 'e':
+                        Image imageBomb=new Image("/img/bomb.png");
+                        ImageView imageViewBomb = new ImageView();
+                        imageViewBomb.setImage(imageBomb);
+                        grid.add(imageViewBomb,j,i);
+                        break;
+                    case 'f':
+                        Image imageSpikes=new Image("/img/spikes.png");
+                        ImageView imageViewSpikes = new ImageView();
+                        imageViewSpikes.setImage(imageSpikes);
+                        grid.add(imageViewSpikes,j,i);
                         break;
                 }
             }
         }
-        this.getChildren().add(grid); 
+        Image imageRotLeft=new Image("/img/rotLeft.png");
+        Button buttonRotLeft=new Button();
+        buttonRotLeft.setGraphic(new ImageView(imageRotLeft));
+        
+        Image imageRotRight=new Image("/img/rotRight.png");
+        Button buttonRotRight=new Button();
+        buttonRotRight.setGraphic(new ImageView(imageRotRight));
+        
+        HBox hb= new HBox();
+        hb.getChildren().add(buttonRotLeft);
+        hb.getChildren().add(buttonRotRight);
+        
+        hb.setAlignment(Pos.CENTER);
+        grid.setPadding(new Insets(10, 10, 10, 10));
+       // this.getChildren().add(grid); 
+        //this.getChildren().add(hb);
+        this.add(grid,0,0);
+        this.add(hb,0,1);
+
     }
 }
